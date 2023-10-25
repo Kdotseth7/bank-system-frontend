237f5896bae893284118d327e0c1010da897a1ce
Kushagra Seth <kseth919@kushagras-mbp.lan>
2023-10-24 22:20:08 -0700

    Structure of Pomelo Txns and Payments

--- src/components/payments/Payments.js
+++ src/components/payments/Payments.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Grid, Paper, Typography} from "@mui/material";
+import paymentsComponent from "../payments/PaymentsComponent.module.scss";
 
 function Payments({ data }) {
     // Filtering only the payment events from the data
@@ -6,17 +8,42 @@ function Payments({ data }) {
     // const postedPayments = data.settledTransactions.filter(txn => txn.amount < 0).slice(0, 3);
 
     return (
-        <div>
-            <h2>Initiated Payments</h2>
-            {/*{initiatedPayments.map(payment => (*/}
-            {/*    <p key={payment.id}>{payment.id}: ${payment.amount} @ time {payment.time}</p>*/}
-            {/*))}*/}
-
-            <h2>Posted Payments</h2>
-            {/*{postedPayments.map(payment => (*/}
-            {/*    <p key={payment.id}>{payment.id}: ${payment.amount} @ time {payment.time} (posted @ time {payment.finalizedTime})</p>*/}
-            {/*))}*/}
-        </div>
+    <Grid container flexDirection="row" className={paymentsComponent.container}>
+        <Grid item>
+            <Paper
+                variant="elevation"
+                square={false}
+                style={{ width: '300px', height: '200px' }} // adjust these values to your preference
+                className={paymentsComponent.paper}
+            >
+                <Grid container flexDirection="column">
+                    <Grid item className={paymentsComponent.paperTitle}>
+                        <Typography variant="h5">Initiated Payments</Typography>
+                        {/*{initiatedPayments.map(payment => (*/}
+                        {/*    <p key={payment.id}>{payment.id}: ${payment.amount} @ time {payment.time}</p>*/}
+                        {/*))}*/}
+                    </Grid>
+                </Grid>
+            </Paper>
+        </Grid>
+        <Grid item>
+            <Paper
+                variant="elevation"
+                square={false}
+                style={{ width: '300px', height: '200px' }} // adjust these values to your preference
+                className={paymentsComponent.paper}
+            >
+                <Grid container flexDirection="column">
+                    <Grid item className={paymentsComponent.paperTitle}>
+                        <Typography variant="h5">Posted Payments</Typography>
+                        {/*{postedPayments.map(payment => (*/}
+                        {/*    <p key={payment.id}>{payment.id}: ${payment.amount} @ time {payment.time} (posted @ time {payment.finalizedTime})</p>*/}
+                        {/*))}*/}
+                    </Grid>
+                </Grid>
+            </Paper>
+        </Grid>
+    </Grid>
     );
 }
 
--- src/components/transactions/Transactions.js
+++ src/components/transactions/Transactions.js
@@ -1,18 +1,45 @@
 import React from 'react';
+import {Grid, Paper, Typography} from "@mui/material"
+import transactionsComponent from "./TransactionsComponent.module.scss";
 
 function Transactions({ data }) {
     return (
-        <div>
-            <h2>Pending Transactions</h2>
-            {/*{data.pendingTransactions.map(txn => (*/}
-            {/*    <p key={txn.id}>{txn.id}: ${txn.amount} @ time {txn.time}</p>*/}
-            {/*))}*/}
-
-            <h2>Settled Transactions</h2>
-            {/*{data.settledTransactions.slice(0, 3).map(txn => (*/}
-            {/*    <p key={txn.id}>{txn.id}: ${txn.amount} @ time {txn.time} (finalized @ time {txn.finalizedTime})</p>*/}
-            {/*))}*/}
-        </div>
+        <Grid container flexDirection="row" className={transactionsComponent.container}>
+            <Grid item>
+                <Paper
+                    variant="elevation"
+                    square={false}
+                    style={{ width: '300px', height: '200px' }} // adjust these values to your preference
+                    className={transactionsComponent.paper}
+                >
+                    <Grid container flexDirection="column">
+                        <Grid item className={transactionsComponent.paperTitle}>
+                            <Typography variant="h5">Pending Transactions</Typography>
+                            {/*{data.pendingTransactions.map(txn => (*/}
+                            {/*    <p key={txn.id}>{txn.id}: ${txn.amount} @ time {txn.time}</p>*/}
+                            {/*))}*/}
+                        </Grid>
+                    </Grid>
+                </Paper>
+            </Grid>
+            <Grid item>
+                <Paper
+                    variant="elevation"
+                    square={false}
+                    style={{ width: '300px', height: '200px' }} // adjust these values to your preference
+                    className={transactionsComponent.paper}
+                >
+                    <Grid container flexDirection="column">
+                        <Grid item className={transactionsComponent.paperTitle}>
+                            <Typography variant="h5">Settled Transactions</Typography>
+                            {/*{data.pendingTransactions.map(txn => (*/}
+                            {/*    <p key={txn.id}>{txn.id}: ${txn.amount} @ time {txn.time}</p>*/}
+                            {/*))}*/}
+                        </Grid>
+                    </Grid>
+                </Paper>
+            </Grid>
+        </Grid>
     );
 }
 
